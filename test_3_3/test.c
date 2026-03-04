602128d983a85139bdebd99927bf80e5689efe8b
三三 <2663307689@qq.com>
2026-03-04 20:02:25 +0800

    hello world

---
+++ test_3_3/test.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main() {
+	printf("hello_world\n");
+	printf("hello_world\n");
+	printf("hello_world\n");
+	printf("hello_world\n");
+	printf("hello_world\n");
+	printf("hello_world\n");
+	printf("hello_world\n");
+	printf("hello_world\n");
+
+
+	return 0;
+}
